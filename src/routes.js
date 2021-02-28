b6f73ffaace8abf096970d6a895823895a5fb3aa
omarqui <omarquiaybar@gmail.com>
2021-02-28 17:07:56 -0400

    refactor: split and reorganize route funtion

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,42 @@
+const addRoute = require('./helpers/addRoute');
+const redirectTo = require('./helpers/redirectTo');
 const home = require('./controllers/home.controller');
-const route404 = require('./controllers/404.controller');
+const controller404 = require('./controllers/404.controller');
 const parcer = require('./controllers/parser.controller');
 
-const redirectTo = location => {
-    return (req, res) => {
-        res.writeHead(302, {
-            'Location': location
-        });
-    };
-}
+const routesList = [];
+const addRouteProxi = (endPoints, constructor) =>
+    addRoute(routesList, endPoints, constructor);
 
-const routes = (req, res) => {
-    let handler;
+addRouteProxi([
+    `/home`,
+    '/index',
+    '/home.html',
+    '/index.html'
+], redirectTo('/'));
+addRouteProxi(['/'], home);
+addRouteProxi(['/404'], controller404);
+addRouteProxi(['/parcer'], parcer);
 
-    switch (req.url) {
-        case '/home':
-        case '/index':
-        case '/home.html':
-        case '/index.html':
-            handler = redirectTo('/')
-            break;
-        case '/':        
-            handler = home;
-            break;
-        case '/404':
-            handler = route404;
-            break;
-        case '/parcer/':
-            handler = parcer;
-            break;
-        default:
-            handler = redirectTo('/404')
-            break;
+const routesHandlers = (req, res) => {
+    let isChanged;
+
+    if (req.method !== 'GET') {
+        redirectTo('/404')(req, res);
+        return;
     }
 
-    handler(req, res);
-    res.end();
-};
+    routesList.map(route => {
+        if (route.endPoints.find(endPoint => req.url === endPoint)) {
+            route.controller(req, res);
+            console.log(route);
+            isChanged = true;
+        }
+    });
 
-module.exports = routes;
+    if (!isChanged) redirectTo('/404')(req, res);
+}
+
+module.exports = {
+    routes: routesHandlers
+}
